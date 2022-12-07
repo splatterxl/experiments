ff46c8f822b96d3ab058aefe533469199bcfb02a
Splatterxl <splatterxl@outlook.ie>
2022-12-07 17:44:20 +0000

    minor fix
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/pages/api/interaction.ts
+++ src/pages/api/interaction.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { webcrypto } from 'crypto';
-import {
+import type {
 	APIChatInputApplicationCommandInteraction,
 	APIInteraction,
 	APIInteractionResponse,
