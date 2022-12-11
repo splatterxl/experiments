0d08f3109081560fda5576e7d24634925dc4eff2
Splatterxl <splatterxl@outlook.ie>
2022-12-11 22:33:58 +0000

    minor layout improvement for footer in mobile
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,20 @@
-import { Heading, HStack, List, ListItem } from '@chakra-ui/react';
+import { Heading, HStack, List, ListItem, Stack } from '@chakra-ui/react';
 import Link from 'next/link';
 
 export const Footer = () => {
 	return (
-		<HStack w='full' borderTopWidth={1} p={6} pr={8} justify='space-between'>
+		<Stack
+			direction={{ base: 'column', md: 'row' }}
+			w='full'
+			borderTopWidth={1}
+			p={6}
+			pr={8}
+			justify='space-between'
+		>
 			<Heading size='sm' fontWeight={500}>
 				&copy; Experiments, 2022. All rights reserved.
 			</Heading>
-			<List fontWeight={400} as={HStack}>
+			<List fontWeight={400} as={HStack} spacing={0} gap={2}>
 				<ListItem>
 					<Link href='/terms'>Terms of Service</Link>
 				</ListItem>
@@ -15,6 +22,6 @@ export const Footer = () => {
 					<Link href='/privacy'>Privacy Policy</Link>
 				</ListItem>
 			</List>
-		</HStack>
+		</Stack>
 	);
 };
