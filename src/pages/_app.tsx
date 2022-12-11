3c62e1c9aaf37b8f12a4d90d0dbf43ba2a902063
Splatterxl <splatterxl@outlook.ie>
2022-12-11 23:00:41 +0000

    this worked for chrysalis let's hope it works for this
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { Footer } from '../components/footer/Footer';
 import Navbar from '../components/navbar/Navbar';
+import { NavigationProgressBar } from '../components/NavigationProgress';
 import { Snowflakes } from '../components/Snowflakes';
 import '../styles/globals.css';
 import { cleanURL } from '../utils/analytics';
@@ -24,6 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				_dark={{ bg: 'linear-gradient(90deg, #1A202C 0%, #1f1b1a 100%)' }}
 				_light={{ bg: 'linear-gradient(90deg, #F7FAFC 0%, #f9f0df 100%)' }}
 			>
+				{/* <NavigationProgressBar /> */}
 				<Box role='main' w='100vw' minH='100vh'>
 					<Navbar />
 					<Snowflakes />
