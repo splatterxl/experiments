aa71cd0e07a5476040cda32eb03436368fe94307
Splatterxl <splatterxl@outlook.ie>
2022-12-18 19:44:52 +0000

    add vercel analytics
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { Box, ChakraProvider, VStack } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { Footer } from '../components/footer/Footer';
 import Navbar from '../components/navbar/Navbar';
-import { NavigationProgressBar } from '../components/NavigationProgress';
 import { Snowflakes } from '../components/Snowflakes';
 import '../styles/globals.css';
 import { cleanURL } from '../utils/analytics';
@@ -18,21 +18,24 @@ export default function App({ Component, pageProps }: AppProps) {
 	}, [router]);
 
 	return (
-		<ChakraProvider theme={THEME}>
-			<VStack
-				minH='100vh'
-				justify='space-between'
-				_dark={{ bg: 'linear-gradient(90deg, #1A202C 0%, #1f1b1a 100%)' }}
-				_light={{ bg: 'linear-gradient(90deg, #F7FAFC 0%, #f9f0df 100%)' }}
-			>
-				{/* <NavigationProgressBar /> */}
-				<Box role='main' w='100vw' minH='100vh'>
-					<Navbar />
-					<Snowflakes />
-					<Component {...pageProps} />
-				</Box>
-				<Footer />
-			</VStack>
-		</ChakraProvider>
+		<>
+			<ChakraProvider theme={THEME}>
+				<VStack
+					minH='100vh'
+					justify='space-between'
+					_dark={{ bg: 'linear-gradient(90deg, #1A202C 0%, #1f1b1a 100%)' }}
+					_light={{ bg: 'linear-gradient(90deg, #F7FAFC 0%, #f9f0df 100%)' }}
+				>
+					{/* <NavigationProgressBar /> */}
+					<Box role='main' w='100vw' minH='100vh'>
+						<Navbar />
+						<Snowflakes />
+						<Component {...pageProps} />
+					</Box>
+					<Footer />
+				</VStack>
+			</ChakraProvider>
+			<Analytics />
+		</>
 	);
 }
