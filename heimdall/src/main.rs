fdc84874bde43e3d26b8fde8d8ae302089bf91d7
Splatterxl <splatterxl@outlook.ie>
2022-12-19 20:55:59 +0000

    he really said
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- heimdall/src/main.rs
+++ heimdall/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, future::IntoFuture};
+use twilight_http::{
+    request::{Method, RequestBuilder},
+    routing::Path,
+    Client,
+};
+use twilight_model::id::Id;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Initialize the tracing subscriber.
+    tracing_subscriber::fmt::init();
+
+    let client = Client::new(env::var("DISCORD_TOKEN")?).await;
+
+    let req = RequestBuilder::raw(
+        Method::Post,
+        Path::Gateway,
+        "https://51b4-2a09-bac1-3500-10-00-34-42.eu.ngrok.io".to_owned(),
+    )
+    .build();
+
+    let _ = client.request::<()>(req).into_future().await?;
+
+    let me = client.current_user().await?.model().await?;
+    println!("Current user: {}#{}", me.name, me.discriminator);
+
+    Ok(())
 }
