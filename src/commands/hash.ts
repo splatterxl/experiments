19d3759ca91cee2dc3e77723051de9219f8ac5c1
splatter <splatterxl@outlook.ie>
2024-08-01 23:24:29 +0100

    add error logging test
    Signed-off-by: splatter <splatterxl@outlook.ie>

--- src/commands/hash.ts
+++ src/commands/hash.ts
@@ -5,6 +5,13 @@ export default async function (i: CommandInteraction) {
   const string = i.options.get("string", true).value as string;
   const hashed = murmur3(string);
 
+  if (
+    string ===
+    "super secret string that literally just crashes the bot, pls do not use it's just for testing"
+  ) {
+    throw new Error("Crash test");
+  }
+
   i.reply({
     content: `Murmur3: \`${hashed.toString()}\`\nRollout position: ${
       hashed % 10e3
